9b66ba36997d35ff5a0552a665ba3f3a098ec918
swardiantara <swardyantara@gmail.com>
2025-11-26 16:25:38 +0700

    BIRCH on k=2

--- scripts/grid_search.sh
+++ scripts/grid_search.sh
@@ -3,12 +3,12 @@
 # source ~/miniconda3/bin/activate sbert
 source ~/anaconda3/bin/activate sbert
 # Apache Android BGL Hadoop HDFS HealthApp HPC Linux Mac OpenSSH OpenStack Proxifier Spark Thunderbird Windows Zookeeper 
-datasets=( MultiUnique )
-# datasets=( Apache Android BGL Hadoop HDFS HealthApp HPC Linux Mac OpenSSH OpenStack Proxifier Spark Thunderbird Windows Zookeeper MultiSource MultiUnique )
 # datasets=( MultiUnique )
-models=( agglomerative )
-# embeddings=( MultiSource-full-crk2-m0.5-e5-b128-L6 MultiSource-full-cdk2-m0.5-e5-b128-L6 ) # one-crk10-m0.5-e5-b128-L6 one-crk10-m0.05-e2-b128-L6 one-cdk10-m0.5-e5-b16-v1 
-embeddings=( MultiSource-full-crk0-m0.5-e5-b128-L6 MultiSource-full-crk1-m0.5-e5-b128-L6 MultiSource-full-cdk1-m0.5-e5-b128-L6 MultiSource-full-crk2-m0.5-e5-b128-L6 MultiSource-full-cdk2-m0.5-e5-b128-L6 MultiSource-full-crk3-m0.5-e5-b128-L6 MultiSource-full-cdk3-m0.5-e5-b128-L6 MultiSource-full-crk5-m0.5-e5-b128-L6 MultiSource-full-cdk5-m0.5-e5-b128-L6 MultiSource-full-crk10-m0.5-e5-b128-L6 MultiSource-full-cdk10-m0.5-e5-b128-L6 ) # one-crk10-m0.5-e5-b128-L6 one-crk10-m0.05-e2-b128-L6 one-cdk10-m0.5-e5-b16-v1 
+datasets=( Apache Android BGL Hadoop HDFS HealthApp HPC Linux Mac OpenSSH OpenStack Proxifier Spark Thunderbird Windows Zookeeper MultiSource MultiUnique )
+# datasets=( MultiUnique )
+models=( birch )
+embeddings=( MultiSource-full-crk2-m0.5-e5-b128-L6 MultiSource-full-cdk2-m0.5-e5-b128-L6 ) # one-crk10-m0.5-e5-b128-L6 one-crk10-m0.05-e2-b128-L6 one-cdk10-m0.5-e5-b16-v1 
+# embeddings=( MultiSource-full-crk0-m0.5-e5-b128-L6 MultiSource-full-crk1-m0.5-e5-b128-L6 MultiSource-full-cdk1-m0.5-e5-b128-L6 MultiSource-full-crk2-m0.5-e5-b128-L6 MultiSource-full-cdk2-m0.5-e5-b128-L6 MultiSource-full-crk3-m0.5-e5-b128-L6 MultiSource-full-cdk3-m0.5-e5-b128-L6 MultiSource-full-crk5-m0.5-e5-b128-L6 MultiSource-full-cdk5-m0.5-e5-b128-L6 MultiSource-full-crk10-m0.5-e5-b128-L6 MultiSource-full-cdk10-m0.5-e5-b128-L6 ) # one-crk10-m0.5-e5-b128-L6 one-crk10-m0.05-e2-b128-L6 one-cdk10-m0.5-e5-b16-v1 
 # thresholds=( 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.70 0.75 0.80 0.85 0.9 0.95 1 )
 thresholds=( 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.11 0.12 0.13 0.14 0.15 0.16 0.17 0.18 0.19 0.2 0.21 0.22 0.23 0.24 0.25 0.26 0.27 0.28 0.29 0.3 )
 for embedding in "${embeddings[@]}"; do
