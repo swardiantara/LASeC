98190eaaafb7f907331d032eb92483b82c6d82f6
swardiantara <swardyantara@gmail.com>
2025-11-27 12:05:04 +0700

    embedding for effect of base models

--- scripts/train_embedding_all.sh
+++ scripts/train_embedding_all.sh
@@ -24,20 +24,20 @@
 #     done
 # done
 
-# initial_models=( all-mpnet-base-v2 ) # effect of different initial models
-# samplings=( distance )
-# num_samples=( 3 )
+initial_models=( all-MiniLM-L6-v2 all-MiniLM-L12-v2 all-distilroberta-v1 all-mpnet-base-v2 ) # effect of different initial models
+samplings=( random )
+num_samples=( 2 )
 
-# for model in "${initial_models[@]}"; do
-#     for k in "${num_samples[@]}"; do
-#         for sampling in "${samplings[@]}"; do
-#             python -m src.train_embedding --initial_model_path "$model" --sampling_strategy "$sampling" --k "$k" --push_embedding --batch_size 16
-#         done
-#     done
-# done
+for model in "${initial_models[@]}"; do
+    for k in "${num_samples[@]}"; do
+        for sampling in "${samplings[@]}"; do
+            python -m src.train_embedding --initial_model_path "$model" --sampling_strategy "$sampling" --k "$k" --push_embedding --batch_size 8
+        done
+    done
+done
 
 # train with 80% of data
-model="all-MiniLM-L6-v2"
-sampling="random"
-k=2
-python -m src.train_embedding --initial_model_path "$model" --sampling_strategy "$sampling" --k "$k" --push_embedding --template_portion partial --dataset MultiSource
+# model="all-MiniLM-L6-v2"
+# sampling="random"
+# k=2
+# python -m src.train_embedding --initial_model_path "$model" --sampling_strategy "$sampling" --k "$k" --push_embedding --template_portion partial --dataset MultiSource
