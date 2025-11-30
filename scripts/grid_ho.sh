0e006b1a495042df0801768404dd8fd5e9a18409
swardiantara <swardyantara@gmail.com>
2025-11-30 17:48:51 +0700

    held-out on k=2

--- scripts/grid_ho.sh
+++ scripts/grid_ho.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-datasets=( Apache Android BGL Hadoop HDFS HealthApp HPC Linux Mac OpenSSH OpenStack Proxifier Spark Thunderbird Windows Zookeeper MultiSource MultiUnique )
+datasets=( MultiSource Apache Android BGL Hadoop HDFS HealthApp HPC Linux Mac OpenSSH OpenStack Proxifier Spark Thunderbird Windows Zookeeper MultiUnique )
 # datasets=( Apache Android )
 # datasets=( MultiUnique )
-models=( agglomerative birch )
+models=( agglomerative )
 # embeddings=( MultiSource-full-crk0-m0.5-e5-b128-L6 MultiSource-full-crk1-m0.5-e5-b128-L6 MultiSource-full-cdk1-m0.5-e5-b128-L6 MultiSource-full-crk3-m0.5-e5-b128-L6 MultiSource-full-cdk3-m0.5-e5-b128-L6 MultiSource-full-crk5-m0.5-e5-b128-L6 MultiSource-full-cdk5-m0.5-e5-b128-L6 MultiSource-full-crk10-m0.5-e5-b128-L6 MultiSource-full-cdk10-m0.5-e5-b128-L6 ) # one-crk10-m0.5-e5-b128-L6 one-crk10-m0.05-e2-b128-L6 one-cdk10-m0.5-e5-b16-v1 
 thresholds=( 0.05 0.1 0.15 0.2 0.25 0.3 0.35 0.4 0.45 0.5 0.55 0.6 0.65 0.70 0.75 0.80 0.85 0.9 0.95 1 )
 # thresholds=( 0.01 0.02 0.03 0.04 0.05 0.06 0.07 0.08 0.09 0.1 0.11 0.12 0.13 0.14 0.15 0.16 0.17 0.18 0.19 0.2 0.21 0.22 0.23 0.24 0.25 0.26 0.27 0.28 0.29 0.3 )
@@ -13,8 +13,8 @@ for embedding in "${embeddings[@]}"; do
     for dataset in "${datasets[@]}"; do
         for model in "${models[@]}"; do
             for threshold in "${thresholds[@]}"; do
-                python -m src.lasec --dataset "$dataset" --embedding "$embedding" --model "$model" --threshold "$threshold" --output_dir grid-50
-                python -m src.lasec --dataset "$dataset" --embedding "$embedding" --model "$model" --threshold "$threshold" --output_dir grid-50-ho --held_out
+                # python -m src.lasec --dataset "$dataset" --embedding "$embedding" --model "$model" --threshold "$threshold" --output_dir grid
+                python -m src.lasec --dataset "$dataset" --embedding "$embedding" --model "$model" --threshold "$threshold" --output_dir grid-ho-k2 --held_out
             done
         done
     done
